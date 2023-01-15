17faa035f9f5457490e1a20631c7de0abec5a7e3
ssd39 <saurabh.sachit39@gmail.com>
2023-01-15 22:38:23 +0530

    fix

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
 const express = require('express')
-const createClient = require('redis').createClient
+const mongo = require('mongodb');
 const app = express()
 const port = process.env.PORT || 3000
-const redisUrl = process.env.REDIS_URL
+const mongoUrl = process.env.MONGO_URL
 
+let client;
+let collection;
 app.use(express.json());
 
 app.post('/:key', async (req, res) => {
     const val = req.body.val
-    const redisCLient = createClient({
-        url: redisUrl
-    });
-    await redisCLient.connect();
-    await redisCLient.set(req.params.key , val);
-    await redisCLient.disconnect();
+    await collection.insertOne({
+        key:req.params.key,
+        val
+    })
     res.send('ok')
 })
 
 app.get('/:key', async (req, res) => {
-    const redisCLient = createClient({
-        url: redisUrl
-    });
-    await redisCLient.connect();
-    const webTriggerUrl = await client.get(req.params.key);
-    console.log(webTriggerUrl)		
-    await redisCLient.disconnect();
+    const webTriggerUrl = (await collection.findOne({ key: req.params.key })).val
     res.send(webTriggerUrl)
 })
 
-app.listen(port, () => {
+app.listen(port, async () => {
+  client = await mongo.MongoClient.connect(mongoUrl)
+  let db = client.db("main");
+  collection = db.createCollection("kv-api")
   console.log(`Example app listening on port ${port}`)
 })
